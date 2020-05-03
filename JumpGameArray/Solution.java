f83a5356aaa9bdc4334cabcbd9a052f76eb36f0b
Anuj Kumar <iitr.anujkumar@gmail.com>
2020-05-03 12:26:16 +0530

    JumpGameArray

--- JumpGameArray/Solution.java
+++ JumpGameArray/Solution.java
@@ -1,4 +1,12 @@
 public class Solution {
     public int canJump(int[] A) {
+        int maxJump = 0, n = A.length;
+        for(int i = 0;i < n;i++) {
+            if(maxJump >= i) {
+                maxJump = Math.max(maxJump,i+A[i]);
+            }
+        }
+        if(maxJump >= n-1)return 1;
+        else return 0;
     }
 }
