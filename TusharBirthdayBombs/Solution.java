b672749fe5413e57513d1d49fa58aa53f9b44329
Anuj Kumar <iitr.anujkumar@gmail.com>
2020-05-03 11:10:51 +0530

    TusharBirthdayBombs

--- TusharBirthdayBombs/Solution.java
+++ TusharBirthdayBombs/Solution.java
@@ -1,4 +1,29 @@
 public class Solution {
     public int[] solve(int A, int[] B) {
+        
+        int minElement = Integer.MAX_VALUE;
+        int minIndex = -1;
+        int n = B.length;
+        for(int i = 0;i < n;i++) {
+            if(minElement > B[i]) {
+                minElement = B[i];
+                minIndex = i;
+            }
+        }
+        int maxLength = A/minElement;
+        int remainingValue = A - (maxLength)*minElement;
+        int [] ans = new int[maxLength];
+        for(int i = 0;i < maxLength;i++) {
+            ans[i] = minIndex;
+        }
+        int k = 0;
+        for(int i = 0;i < minIndex && k < maxLength;i++) {
+            if(B[i]-minElement <= remainingValue && k < maxLength) {
+                ans[k++] = i;
+                remainingValue -= (B[i]-minElement);
+                i--;
+            }
+        }
+        return ans;
     }
 }
