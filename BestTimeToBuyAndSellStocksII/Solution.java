98d818919fbaf3518593c74f790a89bab92a83a3
Anuj Kumar <iitr.anujkumar@gmail.com>
2020-05-12 00:10:33 +0530

    BestTimeToBuyAndSellStocksII

---
+++ BestTimeToBuyAndSellStocksII/Solution.java
@@ -0,0 +1,16 @@
+public class Solution {
+    // DO NOT MODIFY THE ARGUMENTS WITH "final" PREFIX. IT IS READ ONLY
+    public int maxProfit(final int[] A) {
+        int n = A.length;
+        if(n == 0)return 0;
+        int maxProfit = 0;
+        int buyAt = A[0];
+        for(int i = 1;i < n;i++) {
+            if(A[i] > buyAt) {
+                maxProfit += (A[i]-buyAt);
+            }
+            buyAt = A[i];
+        }
+        return maxProfit;
+    }
+}
